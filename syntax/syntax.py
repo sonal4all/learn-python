4fce159aee323862cc2a47965bfc359e2cd407d0
sonal4all <sonalpal7428@gmail.com>
2025-10-21 21:47:55 +0530

    find even odd

--- syntax/syntax.py
+++ syntax/syntax.py
@@ -5,3 +5,10 @@ if age>18:
     print("sonal is eligible for voting")
 else:
     print("sonal is not eligible for voting")
+
+
+a=5
+if a%2==0:
+    print ('number is even')
+else:
+    print ('number is odd')
